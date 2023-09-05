5475df8980b970ff61213226151ed52acaa6abb3
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-05 11:56:35 -0700

    small exports change

--- 3.4-rendering-with-react/exports-from-here.js
+++ 3.4-rendering-with-react/exports-from-here.js
@@ -4,6 +4,10 @@ export { name }
 const writingImplement = 'pen';
 export { writingImplement };
 
+// the not recommended way of doing named exports
+// you can do a named export all on line
+export const website = 'google.com';
+
 // default exports get exported outside of curly braces and imported outside of curly braces
 const number = 7;
 export default number;

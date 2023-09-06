2e75f639c94b6e01cb63a2efc0b322747119010b
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-06 08:02:10 -0700

    fun with state

--- 3.4-rendering-with-react/cheese/src/Header.js
+++ 3.4-rendering-with-react/cheese/src/Header.js
@@ -3,14 +3,28 @@
 // step 2: write the function that returns some basic HTML
 // step 3: export default
 
-import React from 'react';
+import React, { useState } from 'react';
+
+// the 3 things an event listener needs
+// 1. Which element am I listening for an event on?
+// 2. Which event am I listening for? (click, submit, mouseOver, keypress, etc)
+// 3. What should we do when that event happens? (callback function)
 
 // the name of the prop, funFact, matches the name of the prop in App.js where we render the Header component
 function Header({ factAboutCheese, someOtherString }) {
+  // declaring a normal variable
+  // let numberOfClicks = 0;
+  // declaring a state variable
+  const [numberOfClicks, setNumberOfClicks] = useState(1);
+  function logClick() {
+    console.log('clicked the header')
+  }
   return <header>
-    <h1>Cheese!</h1>
-    <p>{factAboutCheese.toUpperCase()} {someOtherString}</p>
-    <p>2 + 3 is {2 + 3}</p>
+    <h1 onClick={logClick}>Cheese!</h1>
+    <p onClick={() => setNumberOfClicks(1)}>{factAboutCheese.toUpperCase()} {someOtherString}</p>
+    <p onClick={() => setNumberOfClicks(numberOfClicks * 2)}>
+      2 + {numberOfClicks} is {2 + numberOfClicks}
+    </p>
   </header>
 }
 

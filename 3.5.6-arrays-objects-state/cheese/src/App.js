2304f971ac0df292369b452f2a3dc6d068f90fd3
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-07 08:03:23 -0700

    arrays and objects in state

--- 3.5.6-arrays-objects-state/cheese/src/App.js
+++ 3.5.6-arrays-objects-state/cheese/src/App.js
@@ -8,12 +8,58 @@ function App() {
   let factAboutCheese = 'cheese is the best';
 
   const [numberOfClicks, setNumberOfClicks] = useState(1);
-  let cheesesArray = [
-    { name: 'gruyere', imageUrl: "https://bovafoods.com/wp-content/uploads/2020/05/DAI292.jpg", deliciousness: 6 },
-    { name: 'cheddar', imageUrl: "https://i0.wp.com/homesteadersofamerica.com/wp-content/uploads/2022/01/How-to-Make-Cheddar-Cheese-17.jpg?resize=1024%2C683&ssl=1", deliciousness: 10 },
-    { name: 'parmesan', imageUrl: "https://vincentsmeatmarket.com/wp-content/uploads/2021/02/Grater-bottle-scaled.jpg", deliciousness: 8 },
-    { name: 'bleu', imageUrl: "https://cheese-etc.co.uk/wp-content/uploads/2020/02/shropshire-blue-cheese.jpeg", deliciousness: 4 },
-  ]
+  const [cheesesArray, setCheesesArray] = useState([
+    {
+      name: 'gruyere',
+      imageUrl: "https://bovafoods.com/wp-content/uploads/2020/05/DAI292.jpg",
+      deliciousness: 6
+    },
+    {
+      name: 'cheddar',
+      imageUrl: "https://i0.wp.com/homesteadersofamerica.com/wp-content/uploads/2022/01/How-to-Make-Cheddar-Cheese-17.jpg?resize=1024%2C683&ssl=1",
+      deliciousness: 10
+    },
+    {
+      name: 'parmesan',
+      imageUrl: "https://vincentsmeatmarket.com/wp-content/uploads/2021/02/Grater-bottle-scaled.jpg",
+      deliciousness: 8
+    },
+    {
+      name: 'bleu',
+      imageUrl: "https://cheese-etc.co.uk/wp-content/uploads/2020/02/shropshire-blue-cheese.jpeg",
+      deliciousness: 4
+    },
+  ]);
+
+  function addACheese() {
+    console.log('we are adding a cheese');
+    // note: .push is not going to work
+    let newCheeses = [...cheesesArray, {
+      name: 'manchego',
+      imageUrl: 'https://images.squarespace-cdn.com/content/v1/58d5711a9f7456928cbaf420/1529692740910-4D47P1844VI8SAWTY61Y/Image+%5BSquare%5D.jpg',
+      deliciousness: 9
+    }];
+    setCheesesArray(newCheeses);
+    // cheesesArray.push({
+    //   name: 'manchego',
+    //   imageUrl: '',
+    //   deliciousness: 9
+    // })
+  }
+  function updateCheeseDeliciousness(cheeseToUpdate) {
+    // want to make a new array that contain mostly the same cheeses
+    // except, for the cheese we want to update, we want to change its deliciousness
+    let newCheeses = cheesesArray.map(
+      ch => ch === cheeseToUpdate ?
+        ({ ...ch, deliciousness: ch.deliciousness + 1 }) :
+        ch
+    )
+    setCheesesArray(newCheeses);
+  }
+  function deleteACheese(cheeseToDelete) {
+    let newCheeses = cheesesArray.filter(ch => ch !== cheeseToDelete);
+    setCheesesArray(newCheeses);
+  }
   // change this array into another array where I've transformed each of the elements
   // let cheesesLIs = cheesesArray.map(ch => <li>{ch}</li>)
   return (
@@ -27,11 +73,18 @@ function App() {
         setNumberOfClicks={setNumberOfClicks}
       />
       <main className="main">
+        <button onClick={addACheese}>Add a Cheese</button>
         <h3>Our Cheeses</h3>
         <ul>
           {/* {cheesesLIs} */}
           {/* the key is something to uniquely identify each LI for React's sake */}
-          {cheesesArray.map(ch => <Cheese cheese={ch} key={ch.name} numberOfClicks={numberOfClicks} />)}
+          {cheesesArray.map(ch => <Cheese
+            cheese={ch}
+            key={ch.name}
+            numberOfClicks={numberOfClicks}
+            deleteACheese={deleteACheese}
+            updateCheeseDeliciousness={updateCheeseDeliciousness}
+          />)}
         </ul>
         <table>
           {/* tables contain a thead and a tbody */}
--- 3.5.6-arrays-objects-state/cheese/src/Cheese.js
+++ 3.5.6-arrays-objects-state/cheese/src/Cheese.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './Cheese.css';
 
-function Cheese({ cheese, numberOfClicks }) {
+function Cheese({ cheese, numberOfClicks, deleteACheese, updateCheeseDeliciousness }) {
   // if you wanted to avoid ternaries, this is how to do it
   let extraString;
   if (cheese.name === 'cheddar') {
@@ -13,12 +13,15 @@ function Cheese({ cheese, numberOfClicks }) {
   // to show and hide the name of the cheese based on clicking the image
   const [shouldShowNameOfCheese, setShouldShowNameOfCheese] = useState(false);
 
+  function handleDeleteButtonClicked() {
+    deleteACheese(cheese);
+  }
+
   // the secret behavior of &&
   // if the first thing is falsy, give back the first thing
   // if the first thing is truthy, give back the second thing
   // so, we can use && when we want to either show or hide something based on a condition
   return <li>
-    <span>Number of clicks: {numberOfClicks} </span>
     <img src={cheese.imageUrl} onClick={() => setShouldShowNameOfCheese(!shouldShowNameOfCheese)} />
 
     {/* if the cheese is cheddar, include the string "the best cheese" */}
@@ -29,6 +32,12 @@ function Cheese({ cheese, numberOfClicks }) {
       {cheese.name === 'cheddar' ? ', the best cheese' : ', a cheese'}
       {cheese.name === 'bleu' && ' (not everyone\'s favorite cheese)'}
     </span> : 'REDACTED'}
+    <div>
+      <button className="btn btn-primary" onClick={() => updateCheeseDeliciousness(cheese)}>More Delicious</button>
+    </div>
+    <div>
+      <button className="btn btn-danger" onClick={handleDeleteButtonClicked}>Delete this cheese!</button>
+    </div>
   </li>
 }
 

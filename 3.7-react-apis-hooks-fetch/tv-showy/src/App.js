6cf72907d448a2e059a90ac5dac743e94a8664be
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-11 06:42:40 -0700

    set up for tv showy demo

---
+++ 3.7-react-apis-hooks-fetch/tv-showy/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>TV Showy</h1>
+      <h4>the finest app for finding information about TV shows</h4>
+    </div>
+  );
+}
+
+export default App;

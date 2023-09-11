869f26ac96b2d9e02bf5b30511484be8e132da09
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-11 06:49:48 -0700

    add in a state variable

--- 3.7-react-apis-hooks-fetch/tv-showy/src/App.js
+++ 3.7-react-apis-hooks-fetch/tv-showy/src/App.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [clicks, setClicks] = useState(0)
   return (
     <div className="container">
-      <h1>TV Showy</h1>
+      <h1 onClick={() => setClicks(clicks + 1)}>TV Showy {clicks}</h1>
       <h4>the finest app for finding information about TV shows</h4>
     </div>
   );

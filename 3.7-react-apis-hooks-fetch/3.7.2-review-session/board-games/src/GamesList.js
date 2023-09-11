566717e468d2e4b6de193cb1092ec3ef23f48db6
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-11 11:00:51 -0700

    board game demo

---
+++ 3.7-react-apis-hooks-fetch/3.7.2-review-session/board-games/src/GamesList.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Game from './Game';
+
+function GamesList() {
+  const [gamesArray, setGamesArray] = useState([
+    {
+      "name": "Sorry!",
+      "imageUrl": "https://images.heb.com/is/image/HEBGrocery/001669716-1",
+      "summary": "knock out others to win!"
+    },
+    {
+      "name": "Citadels",
+      "imageUrl": "https://cf.geekdo-images.com/sHd0jkZZLDgixHjAXtn7kA__imagepage/img/BAc4tOD4A_Bu2QJ2lR_B_zmelto=/fit-in/900x600/filters:no_upscale():strip_icc()/pic3119514.jpg",
+      "summary": "You are competing to build the biggest and best citadel. Each round, you draft a new secret role and attempt to sabotage your opponents."
+    },
+    {
+      "name": "Scrabble",
+      "imageUrl": "https://images.food52.com/KPaH-agchBk1WQhu73hFcWNDjb4=/1500x0/b195ede2-97dd-4c1a-89b5-547ea0ad27ad--2021-0813_ws-game-company_luxury-wooden-game-boards_scrabble_1x1_1x1_rocky-luten_009.jpg",
+      "summary": "A word game in which two to four players score points by placing tiles, each bearing a single letter, onto a game board divided into a 15×15 grid of squares."
+    },
+    {
+      "name": "Risk",
+      "imageUrl": "https://www.hasbro.com/common/productimages/en_US/9678C9300E7B4385B564846FA3465E47/0814e0d5d8144769d4a59f449756bb71e5ac0745.jpg",
+      "summary": "Battle for Middle Earth!"
+    }
+
+  ])
+  return <div>
+    {gamesArray.map(g => <Game key={g.name} gameData={g} />)}
+  </div>
+}
+
+export default GamesList;

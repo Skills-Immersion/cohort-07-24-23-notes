a217ce96a5e303e26b43d0660ec4dd50889517cb
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-11 08:14:24 -0700

    fetch with abortcontroller

--- 3.7-react-apis-hooks-fetch/tv-showy/src/App.js
+++ 3.7-react-apis-hooks-fetch/tv-showy/src/App.js
@@ -20,26 +20,38 @@ function App() {
 
   const [tvShowsArray, setTvShowsArray] = useState([]);
   // fetching TV shows
-  // useEffect(() => {
-  //   // get the data from the API
-  //   fetch('https://api.tvmaze.com/shows')
-  //     // transform the data from a JSON string to an actual array of objects
-  //     .then(response => response.json())
-  //     // do something with the data
-  //     .then(data => setTvShowsArray(data));
-  // }, [])
+  useEffect(() => {
+    // create an abortcontroller
+    const abortcontroller = new AbortController();
+    // get the data from the API
+    fetch('https://api.tvmaze.com/shows', { signal: abortcontroller.signal })
+      // transform the data from a JSON string to an actual array of objects
+      .then(response => response.json())
+      // do something with the data
+      .then(data => setTvShowsArray(data))
+      .catch(err => {
+        if (err.name !== 'AbortError') {
+          throw err;
+        } else {
+          console.log('there was an aborterror');
+        }
+      });
+    // cleanup function: if React needs to remove the component
+    // this function will get called by React
+    return () => abortcontroller.abort();
+  }, [])
 
   // async/await
   // cannot pass an async function into useEffect
   // so, we'll instead write an inner async function and call it
-  useEffect(() => {
-    async function fetchTvShows() {
-      let response = await fetch('https://api.tvmaze.com/shows');
-      let data = await response.json();
-      setTvShowsArray(data);
-    }
-    fetchTvShows();
-  }, [])
+  // useEffect(() => {
+  //   async function fetchTvShows() {
+  //     let response = await fetch('https://api.tvmaze.com/shows');
+  //     let data = await response.json();
+  //     setTvShowsArray(data);
+  //   }
+  //   fetchTvShows();
+  // }, [])
 
 
   return (

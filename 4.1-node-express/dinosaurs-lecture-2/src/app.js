85224174066d7dc7973c2b8acede31c991e58d78
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-09-25 10:25:00 -0700

    set up for afternoon lecture

---
+++ 4.1-node-express/dinosaurs-lecture-2/src/app.js
@@ -0,0 +1,38 @@
+// in import syntax, this would be
+// import express from 'express';
+const express = require('express');
+const app = express();
+
+// middleware and routes go here, in between creating the app variable and exporting it
+
+// I want this non-responsive middleware to run on EVERY request, so, no path
+app.use((req, res, next) => {
+  // non-responsive middleware: does some work and then moves on to the next piece of middleware
+  console.log(`a request came in! ${req.path}`)
+  next();
+})
+
+
+// for each route, the syntax is basically
+// what HTTP method? GET
+// what path? '/'
+// what to do/what to send in the response?
+app.get('/', (req, res, next) => {
+  // responsive middleware: it sends a response
+  res.send('this is the root route of our dinosaurs app! we LOVE dinosaurs very very much')
+})
+
+// a route to send back the current date and time
+app.get('/whatdayisit', (req, res, next) => {
+  // send back what day it is
+  res.send(`today is ${new Date()}`)
+})
+
+// a route to be greeted by a dinosaur
+app.get('/greeting', (req, res, next) => {
+  res.send('the dinosaur says "rawr"')
+})
+
+// in import syntax, this would be
+// export default app;
+module.exports = app;
---
+++ 4.1-node-express/dinosaurs-lecture-2/src/server.js
@@ -0,0 +1,8 @@
+// pull in what port to use from an environment variable (default 5000)
+const { PORT = 8080 } = process.env;
+// gets all of the code we've written in app.js
+const app = require("./app");
+
+const listener = () => console.log(`Listening on Port ${PORT}!`);
+// start the server listening
+app.listen(PORT, listener);

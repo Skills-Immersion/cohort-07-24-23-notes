fe33b3b758ba081053701c9e57d238bc034ba526
kelvin711 <arauzkelvin99@gmail.com>
2023-11-08 13:47:44 -0600

    remove pt1

--- 5.3-data-structures/BST/bst.js
+++ 5.3-data-structures/BST/bst.js
@@ -7,29 +7,75 @@ class BST {
         this.right = null;
     }
 
-    insert(key, value) {
+    insert(keyToInsert, valueToInsert) {
         // check if the tree is empty
         if ( this.key === null ) {
-            this.key === key;
-            this.value === value;
-        } else if ( key < this.key ) { // check if we need to go left
+            this.key === keyToInsert;
+            this.value === valueToInsert;
+        } else if ( keyToInsert < this.key ) { // check if we need to go left
             // check if we cant go left aka new node belongs here
             if ( !this.left ) {
-                this.left = new BST(key, value, this);
+                this.left = new BST(keyToInsert, valueToInsert, this);
             } else {
-                this.left.insert(key, value);
+                this.left.insert(keyToInsert, valueToInsert);
             }
         } else { //check right
             if ( !this.right ) {
-                this.right = new BST(key, value, this);
+                this.right = new BST(keyToInsert, valueToInsert, this);
             } else {
-                this.right.insert(key, value);
+                this.right.insert(keyToInsert, valueToInsert);
             }
         }
         return this;
     }
+
+    find(keyToLookFor) {
+        // did i find the key
+        if ( this.key === keyToLookFor ) {
+            return [this.key, this.value];
+        } else if ( keyToLookFor < this.key && this.left  ) { // do i need to go left and can i go left
+            return this.left.find(keyToLookFor)
+        } else if ( keyToLookFor > this.key && this.right ) {
+            return this.right.find(keyToLookFor)
+        } else {
+            return null
+        }
+    }
+
+    remove(keyToRemove) {
+        // is this node i want to remove
+        if ( this.key === keyToRemove ) {
+            // check if it has two children
+            if ( this.left && this.right ) {
+                // we need to find a node to promote
+                let nodeToPromote = this.right._findMin();
+                // let nodeToPromote = this.left._findMax();
+                // find min on right or find largest on left
+                // change the node
+                this.key = nodeToPromote.key;
+                this.value = nodeToPromote.value;
+                // then remove extra node
+                nodeToPromote.remove(nodeToPromote.key);
+            } else if ( this.left ) {
+                // replace with my left child
+                this._replaceWith(this.left)
+            } else if ( this.right ) {
+                // replace with my right child
+                this._replaceWith(this.right)
+            } else {
+                // replace leaf node with null
+                this._replaceWith(null)
+            }
+        } else if ( keyToRemove < this.key && this.left ) { // traverse
+            return this.left.remove(keyToRemove);
+        } else if ( keyToRemove > this.key && this.right ) {
+            return this.right.remove(keyToRemove)
+        } else {
+            return null;
+        }
+    }
 }
 
-let bst = new BST(11,11)
-bst.insert(7,7).insert(13,13).insert(5,5)
-console.log(bst);
+let bst = new BST(5,5)
+bst.insert(0,0).insert(1,1).insert(2,2).insert(3,3).insert(4,4).insert(6,6).insert(7,7).insert(8,8).insert(9,9)
+bst.remove(0);
